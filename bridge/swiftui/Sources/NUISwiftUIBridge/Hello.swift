91067b125a5a18e1344839f8914c70f295f4ac33
fwcd <fwcdmail@gmail.com>
2023-03-19 03:21:06 +0100

    Make SwiftUI app appear in the Dock

--- bridge/swiftui/Sources/NUISwiftUIBridge/Hello.swift
+++ bridge/swiftui/Sources/NUISwiftUIBridge/Hello.swift
@@ -1,6 +1,16 @@
 import SwiftUI
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // https://stackoverflow.com/questions/68884499/make-swiftui-app-appear-in-the-macos-dock/71177509
+        NSApplication.shared.setActivationPolicy(.regular)
+        NSApplication.shared.activate(ignoringOtherApps: true)
+    }
+}
+
 struct NUIApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
     var body: some Scene {
         WindowGroup {
             Text("Hello world!")
