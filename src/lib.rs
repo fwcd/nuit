4e9238d0722803c3006a4d9d34583bb5ee7a51c2
fwcd <fwcdmail@gmail.com>
2023-03-20 02:09:23 +0100

    Rename ffi_view to c_root

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ pub use nui_shared::*;
 /// Blocks and presents the given view to the user.
 pub fn run_app(view: impl View) {
     let root = NUIRoot::new(view);
-    let ffi_view = CNUIRoot::from(Box::new(root));
+    let c_root = CNUIRoot::from(Box::new(root));
 
     unsafe {
         #[cfg(target_os = "macos")]
         {
-            nui_swiftui_bridge::run_app(&ffi_view);
+            nui_swiftui_bridge::run_app(&c_root);
         }
     }
 }
