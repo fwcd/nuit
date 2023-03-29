3dfa7002eb76a731bbd6240b9021805a1cb1cd0a
fwcd <fwcdmail@gmail.com>
2023-03-30 00:44:33 +0200

    Add configuration infrastructure

---
+++ src/config.rs
@@ -0,0 +1,58 @@
+use nui_shared::View;
+
+use crate::Backend;
+
+pub struct Config<T> {
+    view: T,
+    preferred_backend: Option<Backend>,
+}
+
+impl<T> Config<T> {
+    pub fn builder(view: T) -> ConfigBuilder<T> {
+        view.into()
+    }
+
+    pub fn preferred_backend(&self) -> Option<Backend> {
+        self.preferred_backend
+    }
+
+    pub fn into_view(self) -> T {
+        self.view
+    }
+}
+
+pub struct ConfigBuilder<T> {
+    view: T,
+    preferred_backend: Option<Backend>,
+}
+
+impl<T> ConfigBuilder<T> {
+    pub fn preferred_backend(mut self, backend: Backend) -> Self {
+        self.preferred_backend = Some(backend);
+        self
+    }
+}
+
+impl<T> From<T> for ConfigBuilder<T> {
+    fn from(view: T) -> Self {
+        Self {
+            view,
+            preferred_backend: None,
+        }
+    }
+}
+
+impl<T> From<ConfigBuilder<T>> for Config<T> {
+    fn from(builder: ConfigBuilder<T>) -> Self {
+        Self {
+            view: builder.view,
+            preferred_backend: builder.preferred_backend,
+        }
+    }
+}
+
+impl<T> From<T> for Config<T> where T: View {
+    fn from(view: T) -> Self {
+        Self::builder(view).into()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,38 @@
+mod backend;
+mod config;
+
+pub use backend::*;
+pub use config::*;
+
 pub use nui_shared::*;
 pub use nui_derive::*;
 
-/// Blocks and presents the given view to the user.
-pub fn run_app(view: impl View) {
-    let root = Root::new(view);
-    let c_root = CRoot::from(Box::new(root));
-
-    unsafe {
+impl Default for Backend {
+    fn default() -> Self {
         #[cfg(target_os = "macos")]
-        {
-            nui_swiftui_bridge::run_app(&c_root);
+        return Backend::SwiftUI;
+        #[cfg(not(target_os = "macos"))]
+        return Backend::GTK;
+    }
+}
+
+/// Blocks and presents the given view to the user.
+pub fn run_app<T>(config: impl Into<Config<T>>) where T: View {
+    let config: Config<T> = config.into();
+    let backend = config.preferred_backend().unwrap_or_default();
+    let view = config.into_view();
+    let root = Root::new(view);
+
+    match backend {
+        Backend::SwiftUI => {
+            let c_root = CRoot::from(Box::new(root));
+            #[cfg(target_os = "macos")]
+            unsafe { nui_swiftui_bridge::run_app(&c_root); }
+            #[cfg(not(target_os = "macos"))]
+            panic!("SwiftUI is not supported outside of macOS!")
+        }
+        Backend::GTK => {
+            panic!("GTK is not supported (yet)")
         }
     }
 }
