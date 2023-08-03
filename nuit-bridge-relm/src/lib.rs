d71d1d0ff523f1ac7f17bd1c8979e713f39aa882
fwcd <fwcdmail@gmail.com>
2023-08-03 19:00:28 +0200

    Set up basic window with relm4

--- nuit-bridge-relm/src/lib.rs
+++ nuit-bridge-relm/src/lib.rs
@@ -1,5 +1,36 @@
 use nuit_core::{Root, View};
+use gtk::prelude::*;
+use relm4::prelude::*;
 
-pub fn run_app<T>(root: &Root<T>) where T: View {
-    // TODO
+struct NuitApp<T> {
+    root: Root<T>,
+}
+
+#[relm4::component]
+impl<T> SimpleComponent for NuitApp<T> where T: View + 'static {
+    type Init = Root<T>;
+    type Input = ();
+    type Output = ();
+
+    view! {
+        gtk::Window {
+            set_title: Some("Nuit app"),
+            set_default_size: (640, 480)
+        }
+    }
+
+    fn init(
+        nuit_root: Root<T>,
+        root: &Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let model = NuitApp { root: nuit_root };
+        let widgets = view_output!();
+        ComponentParts { model, widgets }
+    }
+}
+
+pub fn run_app<T>(root: Root<T>) where T: View + 'static {
+    let relm_app = RelmApp::new("nuitapp"); // TODO: Provide a way to customize the app id
+    relm_app.run::<NuitApp<T>>(root);
 }
