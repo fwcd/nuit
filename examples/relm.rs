2458ae12135ddcf6f01fa4c522c0330e64a26b5f
fwcd <fwcdmail@gmail.com>
2023-08-03 18:49:56 +0200

    Add relm example

---
+++ examples/relm.rs
@@ -0,0 +1,25 @@
+#![feature(type_alias_impl_trait, impl_trait_in_assoc_type)]
+
+use nuit::{Text, VStack, View, Bind, Config, Backend};
+
+struct HelloView;
+
+impl Bind for HelloView {}
+
+impl View for HelloView {
+    type Body = impl View;
+
+    fn body(&self) -> Self::Body {
+        VStack::new((
+            Text::new("Hello"),
+            Text::new("World"),
+        ))
+    }
+}
+
+fn main() {
+    nuit::run_app(
+        Config::builder(HelloView)
+            .preferred_backend(Backend::Relm)
+    );
+}
