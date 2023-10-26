6e41fdf14e4da65ada41699d6dfa489c155ffde8
fwcd <fwcdmail@gmail.com>
2023-10-26 17:36:38 +0100

    Implement `Identifiable` for `usize` and `String`

--- nuit-core/src/utils/identifiable.rs
+++ nuit-core/src/utils/identifiable.rs
@@ -3,3 +3,15 @@ use crate::Id;
 pub trait Identifiable {
     fn id(&self) -> Id;
 }
+
+impl Identifiable for usize {
+    fn id(&self) -> Id {
+        Id::index(*self)
+    }
+}
+
+impl Identifiable for String {
+    fn id(&self) -> Id {
+        Id::string(self.clone())
+    }
+}
