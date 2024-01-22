ac63b883995eb51fda0773872317736cab7c56ca
fwcd <fwcd@fwcd.dev>
2024-01-22 18:27:10 +0100

    Use compact, untagged encoding scheme for ids

--- nuit-bridge-swiftui/Sources/NuitBridgeSwiftUI/Utils/Id.swift
+++ nuit-bridge-swiftui/Sources/NuitBridgeSwiftUI/Utils/Id.swift
@@ -1,4 +1,28 @@
 public enum Id: Codable, Hashable {
     case index(value: Int)
     case string(value: String)
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let index = try? container.decode(Int.self) {
+            self = .index(value: index)
+        } else if let value = try? container.decode(String.self) {
+            self = .string(value: value)
+        } else {
+            throw DecodingError.dataCorrupted(.init(
+                codingPath: decoder.codingPath,
+                debugDescription: "Expected id, but found neither integer index nor string"
+            ))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .index(value: let value):
+            try container.encode(value)
+        case .string(value: let value):
+            try container.encode(value)
+        }
+    }
 }
--- nuit-core/src/utils/id.rs
+++ nuit-core/src/utils/id.rs
@@ -1,36 +1,36 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[serde(untagged)]
 pub enum Id {
-    Index { value: usize },
-    String { value: String },
+    Index(usize),
+    String(String),
 }
 
 impl Id {
     pub fn index(value: usize) -> Self {
-        Self::Index { value }
+        Self::Index(value)
     }
 
     pub fn string(value: String) -> Self {
-        Self::String { value }
+        Self::String(value)
     }
 }
 
 impl From<usize> for Id {
     fn from(value: usize) -> Self {
-        Self::Index { value }
+        Self::Index(value)
     }
 }
 
 impl From<String> for Id {
     fn from(value: String) -> Self {
-        Self::String { value }
+        Self::String(value)
     }
 }
 
 impl From<&str> for Id {
     fn from(value: &str) -> Self {
-        Self::String { value: value.to_owned() }
+        Self::String(value.to_owned())
     }
 }
