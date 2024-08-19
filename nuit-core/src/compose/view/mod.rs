aa816c5682b40ea224c2b19a9573a6afdacf579f
fwcd <fwcd@fwcd.dev>
2024-08-20 01:35:25 +0200

    Order view modules lexicographically

--- nuit-core/src/compose/view/mod.rs
+++ nuit-core/src/compose/view/mod.rs
@@ -1,17 +1,17 @@
 mod aggregation;
 mod event;
+mod ext;
 mod layout;
 mod modifier;
 mod shape;
 mod view;
 mod widget;
-mod ext;
 
 pub use aggregation::*;
 pub use event::*;
+pub use ext::*;
 pub use layout::*;
 pub use modifier::*;
 pub use shape::*;
 pub use view::*;
 pub use widget::*;
-pub use ext::*;
