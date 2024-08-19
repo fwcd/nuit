3587a1b71af2db1571d651d63d6eb52764c4fe05
fwcd <fwcd@fwcd.dev>
2024-08-20 01:48:03 +0200

    Accept Into<_> in ViewExt to improve ergonomics

--- nuit-core/src/compose/view/ext.rs
+++ nuit-core/src/compose/view/ext.rs
@@ -5,20 +5,20 @@ pub trait ViewExt: Sized {
         Modified::new(self, modifier)
     }
 
-    fn fill(self, style: Style) -> Modified<Self> {
-        self.modifier(ModifierNode::Fill { style })
+    fn fill(self, style: impl Into<Style>) -> Modified<Self> {
+        self.modifier(ModifierNode::Fill { style: style.into() })
     }
 
-    fn padding(self, insets: Insets) -> Modified<Self> {
-        self.modifier(ModifierNode::Padding { insets })
+    fn padding(self, insets: impl Into<Insets>) -> Modified<Self> {
+        self.modifier(ModifierNode::Padding { insets: insets.into() })
     }
 
     fn position(self, position: Vec2<f64>) -> Modified<Self> {
         self.modifier(ModifierNode::Position { position })
     }
 
-    fn frame(self, frame: Frame) -> Modified<Self> {
-        self.modifier(ModifierNode::Frame { frame })
+    fn frame(self, frame: impl Into<Frame>) -> Modified<Self> {
+        self.modifier(ModifierNode::Frame { frame: frame.into() })
     }
 
     fn on_appear(self, action: impl Fn() + 'static) -> Handler<Self, impl Fn(Event)> {
