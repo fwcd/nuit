c195d2e6aebe610b123b1e7aa620a3e6006c53a7
fwcd <fwcd@fwcd.dev>
2024-08-25 22:13:33 +0200

    Stub out Modified implementation for Adwaita

--- nuit-bridge-adwaita/src/node_widget/mod.rs
+++ nuit-bridge-adwaita/src/node_widget/mod.rs
@@ -3,7 +3,7 @@ mod imp;
 use std::rc::Rc;
 
 use adw::{glib::{self, Object}, gtk::{self, Align, Button, Label, Orientation, Text}, prelude::{BoxExt, ButtonExt, WidgetExt}, subclass::prelude::*};
-use nuit_core::{Event, IdPath, IdPathBuf, Node};
+use nuit_core::{Event, Id, IdPath, IdPathBuf, Identified, Node};
 
 // See https://gtk-rs.org/gtk4-rs/stable/latest/book/g_object_subclassing.html
 
@@ -37,7 +37,19 @@ impl NodeWidget {
         Self::new(node, IdPathBuf::root(), Some(Rc::new(Box::new(fire_event))))
     }
 
-    fn child(&self, node: Node, child_path: &IdPath) -> Self {
+    fn child_with_id(&self, node: Node, id: &Id) -> Self {
+        let imp = imp::NodeWidget::from_obj(self);
+        let id_path = imp.id_path.borrow().child(id.clone());
+        let fire_event = imp.fire_event.borrow().clone();
+
+        Self::new(node, id_path, fire_event)
+    }
+
+    fn child_from_identified(&self, node: &Identified<Node>) -> Self {
+        self.child_with_id(node.value().clone(), node.id())
+    }
+
+    fn child_with_path(&self, node: Node, child_path: &IdPath) -> Self {
         let imp = imp::NodeWidget::from_obj(self);
         let id_path = imp.id_path.borrow().join(child_path);
         let fire_event = imp.fire_event.borrow().clone();
@@ -72,7 +84,7 @@ impl NodeWidget {
                 let button = Button::new();
                 match label.value() {
                     Node::Text { content } => button.set_label(content),
-                    _ => button.set_child(Some(&self.child(label.value().clone(), &IdPathBuf::from(label.id().clone())))),
+                    _ => button.set_child(Some(&self.child_from_identified(&label))),
                 }
                 if let Some(ref fire_event) = *fire_event {
                     let fire_event = fire_event.clone();
@@ -86,18 +98,23 @@ impl NodeWidget {
             Node::HStack { spacing, wrapped } => {
                 let gtk_box = gtk::Box::new(Orientation::Horizontal, *spacing as i32);
                 for (child_path, child) in wrapped.value().children_from(&IdPathBuf::from(wrapped.id().clone())) {
-                    gtk_box.append(&self.child(child.clone(), &child_path))
+                    gtk_box.append(&self.child_with_path(child.clone(), &child_path))
                 }
                 self.append(&gtk_box);
             },
             Node::VStack { spacing, wrapped } => {
                 let gtk_box = gtk::Box::new(Orientation::Vertical, *spacing as i32);
                 for (child_path, child) in wrapped.value().children_from(&IdPathBuf::from(wrapped.id().clone())) {
-                    gtk_box.append(&self.child(child.clone(), &child_path))
+                    gtk_box.append(&self.child_with_path(child.clone(), &child_path))
                 }
                 self.append(&gtk_box);
             },
-            // TODO: Handle other node types
+            Node::Modified { wrapped, modifier: _ } => {
+                // TODO: Implement modifiers
+                eprintln!("Warning: Modifiers are not supported yet and ignored");
+                self.append(&self.child_from_identified(&wrapped))
+            },
+            // TODO: Add remaining node types
             _ => {
                 eprintln!("Warning: Unimplemented node type {node:?}");
             },
