3ca6602890627f0e5a12d345e8409c93f10988c3
fwcd <fwcd@fwcd.dev>
2024-08-25 18:20:40 +0200

    Implement basic adwaita example

--- nuit-bridge-adwaita/src/lib.rs
+++ nuit-bridge-adwaita/src/lib.rs
@@ -1,4 +1,4 @@
-use adw::{prelude::*, Application, ApplicationWindow};
+use adw::{gtk::{Box, ListBox, Orientation, SelectionMode}, prelude::*, ActionRow, Application, ApplicationWindow, HeaderBar};
 use nuit_core::{Root, View};
 
 pub fn run_app<T>(root: Root<T>) where T: View + 'static {
@@ -7,11 +7,34 @@ pub fn run_app<T>(root: Root<T>) where T: View + 'static {
         .build();
 
     app.connect_activate(|app| {
+        let row = ActionRow::builder()
+            .activatable(true)
+            .title("Click me")
+            .build();
+        row.connect_activated(|_| {
+            eprintln!("Clicked!");
+        });
+
+        let list = ListBox::builder()
+            .margin_top(32)
+            .margin_end(32)
+            .margin_bottom(32)
+            .margin_start(32)
+            .selection_mode(SelectionMode::None)
+            .css_classes(vec!["boxed-list".to_owned()])
+            .build();
+        list.append(&row);
+
+        let content = Box::new(Orientation::Vertical, 0);
+        content.append(&HeaderBar::new());
+        content.append(&list);
+
         let window = ApplicationWindow::builder()
             .application(app)
             .title("Nuit App")
             .default_width(640)
             .default_height(480)
+            .content(&content)
             .build();
         window.present();
     });
