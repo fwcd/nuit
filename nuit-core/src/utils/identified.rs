6b217dc32ee32e5159b8e0f3b05fb3e19dbab3d5
fwcd <fwcd@fwcd.dev>
2024-09-04 18:37:29 +0200

    Add some more utility methods to Identified

--- nuit-core/src/utils/identified.rs
+++ nuit-core/src/utils/identified.rs
@@ -11,16 +11,35 @@ pub struct Identified<T> {
 }
 
 impl<T> Identified<T> {
+    /// Creates a new `Identified<T>` identifying the given value with the given id.
     pub fn new(id: &Id, value: T) -> Self {
         Self { id: id.clone(), value }
     }
 
+    /// The id.
     pub fn id(&self) -> &Id {
         &self.id
     }
 
+    /// The wrapped value.
     pub fn value(&self) -> &T {
         &self.value
+    }
+
+    /// Consumes and returns the value.
+    pub fn into_value(self) -> T {
+        self.value
+    }
+
+    /// Converts from an `&Identified<T>` to an `Identified<&T>`. Note that
+    /// this involves cloning the id, which may be undesirable if expensive.
+    pub fn as_ref(&self) -> Identified<&T> {
+        Identified { id: self.id.clone(), value: &self.value }
+    }
+
+    /// Maps the given function over the value.
+    pub fn map<U>(self, f: impl FnOnce(T) -> U) -> Identified<U> {
+        Identified { id: self.id, value: f(self.value) }
     }
 }
 
