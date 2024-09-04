31237a8adfbb8ed60746e904e13a57f1277c4bfe
fwcd <fwcd@fwcd.dev>
2024-09-04 02:57:52 +0200

    Prefix SwiftUI ffi entry point with nuit_bridge_swiftui
    This is to avoid potential naming clashes e.g. with an app-level run_app
    function.

--- nuit-bridge-swiftui/Sources/NuitBridgeSwiftUI/NuitBridgeSwiftUI.swift
+++ nuit-bridge-swiftui/Sources/NuitBridgeSwiftUI/NuitBridgeSwiftUI.swift
@@ -1,7 +1,7 @@
 import CNuitBridgeSwiftUI
 import Foundation
 
-@_cdecl("run_app")
+@_cdecl("nuit_bridge_swiftui_run_app")
 func runApp(cRoot: UnsafePointer<CRoot>) {
     NuitApp.root = Root(cRoot: cRoot)
     cRoot.pointee.set_update_callback(cRoot) {
--- nuit-bridge-swiftui/rust/lib.rs
+++ nuit-bridge-swiftui/rust/lib.rs
@@ -2,5 +2,6 @@ use nuit_core::CRoot;
 
 #[cfg(target_vendor = "apple")]
 extern "C" {
+    #[link_name = "nuit_bridge_swiftui_run_app"]
     pub fn run_app(root: *const CRoot);
 }
