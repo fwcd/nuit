fe3094b275c3d50c4398746e04b53d6f53be3c6a
fwcd <fwcd@fwcd.dev>
2024-09-05 23:07:20 +0200

    Set up gesture event infrastructure

--- nuit-core/src/event/event.rs
+++ nuit-core/src/event/event.rs
@@ -2,12 +2,15 @@ use serde::{Serialize, Deserialize};
 
 use crate::Id;
 
+use super::GestureEvent;
+
 /// A UI event.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "camelCase", rename_all_fields = "camelCase")]
 pub enum Event {
     // Interaction
     Click {},
+    Gesture { gesture: GestureEvent },
     UpdateText { content: String },
     UpdatePickerSelection { id: Id },
 
