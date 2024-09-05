97090294747c3ae8999902a36ea7801c1a0ea7bc
fwcd <fwcd@fwcd.dev>
2024-09-06 01:16:33 +0200

    Add accessors to DragEvent

--- nuit-core/src/event/gesture/drag.rs
+++ nuit-core/src/event/gesture/drag.rs
@@ -9,3 +9,20 @@ pub struct DragEvent {
     start_location: Vec2<f64>,
     location: Vec2<f64>,
 }
+
+impl DragEvent {
+    /// The starting point of the drag.
+    pub fn start_location(&self) -> Vec2<f64> {
+        self.start_location
+    }
+
+    /// The current point of the drag.
+    pub fn location(&self) -> Vec2<f64> {
+        self.location
+    }
+
+    /// The distance moved during the gesture.
+    pub fn translation(&self) -> Vec2<f64> {
+        self.location - self.start_location
+    }
+}
