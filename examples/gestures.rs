3eab04268b8a8509b69064f00673bbb00629584c
fwcd <fwcd@fwcd.dev>
2024-09-06 00:12:29 +0200

    Factor out TapView in gestures example

--- examples/gestures.rs
+++ examples/gestures.rs
@@ -1,21 +1,23 @@
 #![feature(type_alias_impl_trait, impl_trait_in_assoc_type)]
 
-use nuit::{Bind, Circle, Color, ShapeExt, State, Text, View, ViewExt};
+use nuit::{Bind, Circle, Color, HStack, ShapeExt, State, Text, View, ViewExt};
 
 #[derive(Bind)]
-struct GesturesView {
+struct TapView {
+    taps: usize,
     color: State<Color>,
 }
 
-impl GesturesView {
-    pub fn new() -> Self {
+impl TapView {
+    pub fn new(taps: usize) -> Self {
         Self {
+            taps,
             color: State::new(Color::BLACK),
         }
     }
 }
 
-impl View for GesturesView {
+impl View for TapView {
     type Body = impl View;
 
     fn body(&self) -> Self::Body {
@@ -24,12 +26,26 @@ impl View for GesturesView {
             .fill(color.get())
             .frame(100)
             .overlay(Text::new("Tap me!"))
-            .on_tap(move || {
+            .on_taps(self.taps, move || {
                 color.set(Color::random_rgb());
             })
     }
 }
 
+#[derive(Bind)]
+struct GesturesView;
+
+impl View for GesturesView {
+    type Body = impl View;
+
+    fn body(&self) -> Self::Body {
+        HStack::new((
+            TapView::new(1),
+            TapView::new(2),
+        ))
+    }
+}
+
 fn main() {
-    nuit::run_app(GesturesView::new());
+    nuit::run_app(GesturesView);
 }
