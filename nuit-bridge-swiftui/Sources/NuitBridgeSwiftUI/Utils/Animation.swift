79abea8764e7b0550c062586830bb04bee549e0a
fwcd <fwcd@fwcd.dev>
2024-09-06 03:44:18 +0200

    Add some more animations

--- nuit-bridge-swiftui/Sources/NuitBridgeSwiftUI/Utils/Animation.swift
+++ nuit-bridge-swiftui/Sources/NuitBridgeSwiftUI/Utils/Animation.swift
@@ -1,3 +1,13 @@
+import Foundation
+
 enum Animation: Codable, Hashable {
     case `default`
+    case curve(curve: Curve, durationSeconds: TimeInterval?)
+
+    enum Curve: String, Codable, Hashable {
+        case linear
+        case easeIn
+        case easeOut
+        case easeInOut
+    }
 }
--- nuit-core/src/utils/animation.rs
+++ nuit-core/src/utils/animation.rs
@@ -1,9 +1,56 @@
+use std::time::Duration;
+
 use serde::{Deserialize, Serialize};
 
+/// An animation to use for smooth view transitions.
 #[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase", rename_all_fields = "camelCase")]
 pub enum Animation {
     Default {},
+    Curve { curve: AnimationCurve, duration_seconds: Option<f64> },
+    // TODO: Springs
+}
+
+/// The curve of a simple animation.
+#[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase", rename_all_fields = "camelCase")]
+pub enum AnimationCurve {
+    Linear,
+    EaseIn,
+    EaseOut,
+    EaseInOut,
+}
+
+impl Animation {
+    pub const LINEAR: Self = Self::Curve { curve: AnimationCurve::Linear, duration_seconds: None };
+    pub const EASE_IN: Self = Self::Curve { curve: AnimationCurve::EaseIn, duration_seconds: None };
+    pub const EASE_OUT: Self = Self::Curve { curve: AnimationCurve::EaseOut, duration_seconds: None };
+    pub const EASE_IN_OUT: Self = Self::Curve { curve: AnimationCurve::EaseInOut, duration_seconds: None };
+
+    /// Creates an animation with the given curve and the given duration (or default if none).
+    pub fn curve(curve: AnimationCurve, duration: Option<Duration>) -> Self {
+        Self::Curve { curve, duration_seconds: duration.map(|d| d.as_secs_f64()) }
+    }
+
+    /// Creates an animation with a linear curve and the given duration (or default if none).
+    pub fn linear(duration: Option<Duration>) -> Self {
+        Self::curve(AnimationCurve::Linear, duration)
+    }
+
+    /// Creates an animation with an ease-in curve and the given duration (or default if none).
+    pub fn ease_in(duration: Option<Duration>) -> Self {
+        Self::curve(AnimationCurve::EaseIn, duration)
+    }
+
+    /// Creates an animation with an ease-out curve and the given duration (or default if none).
+    pub fn ease_out(duration: Option<Duration>) -> Self {
+        Self::curve(AnimationCurve::EaseOut, duration)
+    }
+
+    /// Creates an animation with an ease-in/out curve and the given duration (or default if none).
+    pub fn ease_in_out(duration: Option<Duration>) -> Self {
+        Self::curve(AnimationCurve::EaseInOut, duration)
+    }
 }
 
 impl Default for Animation {
