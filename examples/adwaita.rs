63d6964670c8dddb7b34b711aa752e958a7ca991
fwcd <fwcd@fwcd.dev>
2024-09-06 04:54:43 +0200

    Simplify adwaita example
    To avoid duplication

--- examples/adwaita.rs
+++ examples/adwaita.rs
@@ -1,29 +1,10 @@
 #![feature(type_alias_impl_trait, impl_trait_in_assoc_type)]
 
-use nuit::{Backend, Bind, Button, ConfigBuilder, State, Text, VStack, View};
-
-#[derive(Bind, Default)]
-struct CounterView {
-    count: State<i32>,
-}
-
-impl View for CounterView {
-    type Body = impl View;
-
-    fn body(&self) -> Self::Body {
-        let count = self.count.clone();
-        VStack::new((
-            Text::new(format!("Count: {}", count.get())),
-            Button::new(Text::new("Increment"), move || {
-                count.set(count.get() + 1);
-            })
-        ))
-    }
-}
+use nuit::{Backend, ConfigBuilder, Text};
 
 fn main() {
     nuit::run_app(
-        ConfigBuilder::from(CounterView::default())
+        ConfigBuilder::from(Text::new("Hello Adwaita!"))
             .preferred_backend(Backend::Adwaita)
     );
 }
