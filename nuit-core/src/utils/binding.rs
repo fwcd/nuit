edf0ebe12adf8ff420091757885a95c4363b01e4
fwcd <fwcd@fwcd.dev>
2024-09-07 05:04:47 +0200

    Add some doc comments to Binding

--- nuit-core/src/utils/binding.rs
+++ nuit-core/src/utils/binding.rs
@@ -8,21 +8,25 @@ pub struct Binding<T> {
 }
 
 impl<T> Binding<T> where T: 'static {
+    /// Creates a new binding with the given getter and setter.
     pub fn new(get: impl Fn() -> T + 'static, set: impl Fn(T) + 'static) -> Self {
         Self { get: Rc::new(get), set: Rc::new(set) }
     }
 
+    /// Fetches the referenced value.
     #[must_use]
     pub fn get(&self) -> T {
         (self.get)()
     }
 
+    /// Sets the referenced value.
     pub fn set(&self, value: T) {
         (self.set)(value);
     }
 }
 
 impl<T> Binding<T> where T: Clone + 'static {
+    /// Creates a constant binding with an empty setter.
     pub fn constant(value: T) -> Self {
         Self::new(move || value.clone(), |_| {})
     }
