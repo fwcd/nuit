b69178ea650c7505bfd5c3d6c69aca70c35aa26b
fwcd <fwcd@fwcd.dev>
2024-09-09 03:14:47 +0200

    Add more color conversions

--- nuit-core/src/utils/color.rs
+++ nuit-core/src/utils/color.rs
@@ -38,16 +38,65 @@ impl Color {
         Self::new(red, green, blue, alpha)
     }
 
-    /// Creates a new color with the given RGB components and alpha 1.
+    /// Creates a new color with the given RGB components and no transparency.
     #[must_use]
     pub const fn with_rgb(red: f64, green: f64, blue: f64) -> Self {
         Self::with_rgba(red, green, blue, 1.0)
     }
 
-    /// Creates a new color with the given grayscale value and alpha 1.
+    /// Creates a new color with the given grayscale value and no transparency.
     #[must_use]
     pub const fn with_grayscale(gray: f64) -> Self {
         Self::with_rgb(gray, gray, gray)
+    }
+
+    /// Creates a new color with the given RGBA u8 components as no transparency.
+    #[must_use]
+    pub const fn with_rgba_u8(red: u8, green: u8, blue: u8, alpha: u8) -> Self {
+        Self::with_rgba(
+            red as f64 / 255.0,
+            green as f64 / 255.0,
+            blue as f64 / 255.0,
+            alpha as f64 / 255.0,
+        )
+    }
+
+    /// Creates a new color with the given RGB u8 components as no transparency.
+    #[must_use]
+    pub const fn with_rgb_u8(red: u8, green: u8, blue: u8) -> Self {
+        Self::with_rgba_u8(red, green, blue, 255)
+    }
+
+    /// Creates a new color from the given RGBA u32 value.
+    #[must_use]
+    pub const fn from_rgba_u32(rgba: u32) -> Self {
+        Self::with_rgba_u8(
+            ((rgba >> 24) & 0xFF) as u8,
+            ((rgba >> 16) & 0xFF) as u8,
+            ((rgba >> 8) & 0xFF) as u8,
+            (rgba & 0xFF) as u8,
+        )
+    }
+
+    /// Creates a new color from the given RGB u32 value.
+    #[must_use]
+    pub const fn from_rgb_u32(rgba: u32) -> Self {
+        Self::with_rgb_u8(
+            ((rgba >> 16) & 0xFF) as u8,
+            ((rgba >> 8) & 0xFF) as u8,
+            (rgba & 0xFF) as u8,
+        )
+    }
+
+    /// Creates a new color from the given ARGB u32 value.
+    #[must_use]
+    pub const fn from_argb_u32(argb: u32) -> Self {
+        Self::with_rgba_u8(
+            ((argb >> 16) & 0xFF) as u8,
+            ((argb >> 8) & 0xFF) as u8,
+            (argb & 0xFF) as u8,
+            ((argb >> 24) & 0xFF) as u8,
+        )
     }
 
     /// A random RGBA color.
@@ -112,6 +161,76 @@ impl Color {
     #[must_use]
     pub fn invert_rgb(self) -> Self {
         Self::with_rgba(1.0 - self.red, 1.0 - self.green, 1.0 - self.blue, self.alpha)
+    }
+
+    /// The red component.
+    #[must_use]
+    pub const fn red(self) -> f64 {
+        self.red
+    }
+
+    /// The green component.
+    #[must_use]
+    pub const fn green(self) -> f64 {
+        self.green
+    }
+
+    /// The blue component.
+    #[must_use]
+    pub const fn blue(self) -> f64 {
+        self.blue
+    }
+
+    /// The alpha component.
+    #[must_use]
+    pub const fn alpha(self) -> f64 {
+        self.alpha
+    }
+
+    /// The red component as a byte.
+    #[must_use]
+    #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+    pub const fn red_u8(self) -> u8 {
+        (self.red * 255.0) as u8
+    }
+
+    /// The green component as a byte.
+    #[must_use]
+    #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+    pub const fn green_u8(self) -> u8 {
+        (self.green * 255.0) as u8
+    }
+
+    /// The blue component as a byte.
+    #[must_use]
+    #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+    pub const fn blue_u8(self) -> u8 {
+        (self.blue * 255.0) as u8
+    }
+
+    /// The alpha component as a byte.
+    #[must_use]
+    #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+    pub const fn alpha_u8(self) -> u8 {
+        (self.alpha * 255.0) as u8
+    }
+
+    /// The color as u32 RGB value.
+    #[must_use]
+    pub const fn to_rgb_u32(self) -> u32 {
+        ((self.red_u8() as u32) << 16) | ((self.green_u8() as u32) << 8) | self.blue_u8() as u32
+    }
+
+    /// The color as u32 RGBA value.
+    #[must_use]
+    pub const fn to_rgba_u32(self) -> u32 {
+        ((self.red_u8() as u32) << 24) | ((self.green_u8() as u32) << 16) | (self.blue_u8() as u32) << 8 | self.alpha_u8() as u32
+    }
+
+    /// The color as u32 ARGB value.
+    #[must_use]
+    pub const fn to_argb_u32(self) -> u32 {
+        ((self.alpha_u8() as u32) << 24) | ((self.red_u8() as u32) << 16) | (self.green_u8() as u32) << 8 | self.blue_u8() as u32
     }
 }
 
